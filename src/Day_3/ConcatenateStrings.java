0743699bdaf460825e94f50291962cc5d6ac99c8
Hovhannes <yepremyanhovhannes11@gmail.com>
2021-04-28 18:12:57 +0400

    ConcatenateStrings

---
+++ src/Day_3/ConcatenateStrings.java
@@ -0,0 +1,9 @@
+package Day_3;
+
+public class ConcatenateStrings {
+    public static void main(String[] args) {
+        String text1 = "Hovh";
+        String text2 = "annes";
+        System.out.println(text1 + text2);
+    }
+}
--- src/Day_3/IntToChar.java
+++ src/Day_3/IntToChar.java
@@ -1,2 +1,13 @@
-package Day_3;public class IntToChar {
+package Day_3;
+
+import java.util.Scanner;
+
+public class IntToChar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input int in range 32 to 127");
+        int num = scanner.nextInt();
+        char char1 = (char)num;
+        System.out.println(char1 + "-" + num);
+    }
 }
