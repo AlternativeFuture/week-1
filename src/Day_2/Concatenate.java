58661cbf8764b63fac5576f280d05ca7f4fff988
Hovhannes <yepremyanhovhannes11@gmail.com>
2021-04-28 18:33:09 +0400

    Try

---
+++ src/Day_2/Concatenate.java
@@ -0,0 +1,11 @@
+package Day_2;
+
+public class Concatenate {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        String text = "Gago";
+        System.out.println(a + b);
+        System.out.println(text + a);
+    }
+}
